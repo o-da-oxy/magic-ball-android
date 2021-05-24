d5b563985131da8e71643a325bce39157b4fb5a1
Oksana <demskaya01@gmail.com>
2021-05-25 00:40:57 +0300

    animation shake; toast after one answer

--- app/src/main/java/com/example/magicball/MainActivity.kt
+++ app/src/main/java/com/example/magicball/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.magicball
 
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var alreadyAsked = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +20,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun answer() {
+
+        if (alreadyAsked) {
+            var toast: Toast = Toast.makeText(this, R.string.oneQuestionOnly, Toast.LENGTH_LONG)
+            toast.show()
+            finish()
+            return
+        }
+
         var messages = resources.getStringArray(R.array.messages) //array of mes
         var randomIndex: Int = Random.nextInt(messages.size)
         var msg = messages[randomIndex]
@@ -25,10 +35,11 @@ class MainActivity : AppCompatActivity() {
         var fadeInAnim = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
         message.startAnimation(fadeInAnim)
         //анимация тряска шарика
-
+        val shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake)
+        screen.startAnimation(shakeAnim)
         //передаем значение текстовому элементу экрана
         message.text = msg
-
+        alreadyAsked = true;
     }
     
 }
